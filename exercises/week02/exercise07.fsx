dd496e9756a17a15be5cd6849ca2447956811094
Tom Gardiner <tombofry@hotmail.co.uk>
2017-04-17 15:52:04 +0100

    Add complete solution for Week 2 / Exercise 7

--- exercises/week02/exercise07.fsx
+++ exercises/week02/exercise07.fsx
@@ -12,3 +12,31 @@
 
 type Month =
  | January
+ | February
+ | March
+ | April
+ | May
+ | June
+ | July
+ | August
+ | September
+ | October
+ | November
+ | December
+
+let daysInMonth month year =
+ match month with
+ | February when (year % 4 = 0) -> 29
+ | February -> 28
+ | April | June | September | November -> 30
+ | _ -> 31
+
+// Test:
+
+let resultA = daysInMonth January 2005
+let resultB = daysInMonth February 2006
+let resultC = daysInMonth February 2008
+let resultD = daysInMonth April 1996
+
+// Expected: `31, 28, 29, 30`
+printfn "%i, %i, %i, %i" resultA resultB resultC resultD
